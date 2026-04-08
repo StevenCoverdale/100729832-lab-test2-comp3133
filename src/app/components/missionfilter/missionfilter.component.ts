ab72374fd83ef7669e544f1fe5179037d187e4a4
Steven <s.coverdale@hotmail.com>
2026-04-08 17:53:52 -0400

    implemented missionfilter component

---
+++ src/app/components/missionfilter/missionfilter.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { SpacexService } from '../../services/spacex.service';
+
+@Component({
+  selector: 'app-missionfilter',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatButtonModule
+  ],
+  templateUrl: './missionfilter.component.html',
+  styleUrls: ['./missionfilter.component.scss']
+})
+export class MissionfilterComponent {
+  form = new FormGroup({
+    year: new FormControl<string | null>(null)
+  });
+
+  years = ['2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020'];
+
+  constructor(private spacexService: SpacexService) {}
+
+  onFilter() {
+    const year = this.form.value.year;
+
+    if (!year) {
+      this.spacexService.getAllLaunches().subscribe(launches =>
+        this.spacexService.launchesSignal.set(launches)
+      );
+      return;
+    }
+
+    this.spacexService.getLaunchesByYear(year).subscribe(launches =>
+      this.spacexService.launchesSignal.set(launches)
+    );
+  }
+
+  onClear() {
+    this.form.reset();
+    this.onFilter();
+  }
+}
