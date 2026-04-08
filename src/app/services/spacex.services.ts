8c357e7f0c227af049937d307558d804e04543b1
Steven <s.coverdale@hotmail.com>
2026-04-08 16:57:22 -0400

    implement spaceX API service with signals

---
+++ src/app/services/spacex.services.ts
@@ -0,0 +1,28 @@
+import { Injectable, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Launch } from '../models/launch.model';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class SpacexService {
+  private baseUrl = 'https://api.spacexdata.com/v3';
+
+  launchesSignal = signal<Launch[]>([]);
+  selectedLaunchSignal = signal<Launch | null>(null);
+
+  constructor(private http: HttpClient) {}
+
+  getAllLaunches(): Observable<Launch[]> {
+    return this.http.get<Launch[]>(`${this.baseUrl}/launches`);
+  }
+
+  getLaunchesByYear(year: string): Observable<Launch[]> {
+    return this.http.get<Launch[]>(`${this.baseUrl}/launches`, {
+      params: { launch_year: year }
+    });
+  }
+
+  getLaunchByFlightNumber(flightNumber: string): Observable<Launch> {
+    return this.http.get<Launch>(`${this.baseUrl}/launches/${flightNumber}`);
+  }
+}
