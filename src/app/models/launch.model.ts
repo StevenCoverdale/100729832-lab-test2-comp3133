eaa9e0ab4268e67f93e8ea633f973b60dc145d6c
Steven <s.coverdale@hotmail.com>
2026-04-08 16:56:05 -0400

    Added Launch model

---
+++ src/app/models/launch.model.ts
@@ -0,0 +1,21 @@
+export interface Rocket {
+  rocket_id: string;
+  rocket_name: string;
+  rocket_type: string;
+}
+
+export interface Links {
+  mission_patch_small: string | null;
+  article_link: string | null;
+  wikipedia: string | null;
+  video_link: string | null;
+}
+
+export interface Launch {
+  flight_number: number;
+  mission_name: string;
+  launch_year: string;
+  details: string | null;
+  rocket: Rocket;
+  links: Links;
+}
